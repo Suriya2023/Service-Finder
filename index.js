14282ac5d5e63976edd001fa61ce0ca34efa8002
Suriya2023 <rajputsuraj221221@gmail.com>
2024-05-25 17:42:04 +0530

    add index file and multiples pages attach with site

---
+++ index.js
@@ -0,0 +1,169 @@
+function focusOnEvent(){
+    // var event = document.getElementsByClassName('form-control');
+    // .focus();
+    if (condition) {
+        
+    } else {
+        
+    }
+}
+
+
+function MainBoxDivLi(){
+    if (document.getElementById('boxDiveVisibility').style.display == "block") {
+                    document.getElementById('boxDiveVisibility').style.display = "none"
+    } else {
+                    document.getElementById('boxDiveVisibility').style.display = "block"
+    }
+}
+function MainBoxDivLipop(){
+    if (document.getElementById('boxDiveVisibility2').style.display == "block") {
+                    document.getElementById('boxDiveVisibility2').style.display = "none"
+    } else {
+                    document.getElementById('boxDiveVisibility2').style.display = "block"
+    }
+}
+
+
+function ProviderCostomer(){
+    if (document.getElementById('box-sing-up').style.display == "block") {
+            document.getElementById('box-sing-up').style.display="none"
+            
+    } else {
+            window.document.getElementById('box-sing-up').style.display ="block"
+    }
+}
+function MainBoxDivLilast(){
+    if (document.getElementById('boxDiveVisibilitylast').style.display == "block") {
+            document.getElementById('boxDiveVisibilitylast').style.display = "none"
+            
+    } else {
+            document.getElementById('boxDiveVisibilitylast').style.display = "block"
+    }
+}
+function onMyiconhome(){
+
+    if (document.getElementById('myonMyiconhome').style.display == "block") {
+            document.getElementById('myonMyiconhome').style.display = "none";
+            document.getElementById('P2').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhome').style.display = "block";
+            document.getElementById('P2').style.color="#FFB600";
+
+    }
+    
+}
+function onMyiconhometwo(){
+
+    if (document.getElementById('myonMyiconhometwo').style.display == "block") {
+            document.getElementById('myonMyiconhometwo').style.display = "none";
+            document.getElementById('P3').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhometwo').style.display = "block";
+            document.getElementById('P3').style.color="#FFB600";
+
+    }
+    
+}
+
+function onMyiconhomethe(){
+
+    if (document.getElementById('myonMyiconhomethe').style.display == "block") {
+            document.getElementById('myonMyiconhomethe').style.display = "none";
+            document.getElementById('P4').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhomethe').style.display = "block";
+            document.getElementById('P4').style.color="#FFB600";
+
+    }
+    
+}
+
+function onMyiconhomefor(){
+
+    if (document.getElementById('myonMyiconhomefor').style.display == "block") {
+            document.getElementById('myonMyiconhomefor').style.display = "none";
+            document.getElementById('P5').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhomefor').style.display = "block";
+            document.getElementById('P5').style.color="#FFB600";
+
+    }
+    
+}
+function onMyiconhomefive(){
+
+    if (document.getElementById('myonMyiconhomefive').style.display == "block") {
+            document.getElementById('myonMyiconhomefive').style.display = "none";
+            document.getElementById('P6').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhomefive').style.display = "block";
+            document.getElementById('P6').style.color="#FFB600";
+
+    }
+    
+}
+function onMyiconhomesix(){
+
+    if (document.getElementById('myonMyiconhomesix').style.display == "block") {
+            document.getElementById('myonMyiconhomesix').style.display = "none";
+            document.getElementById('P7').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhomesix').style.display = "block";
+            document.getElementById('P7').style.color="#FFB600";
+
+    }
+    
+}
+function onMyiconhomeseven(){
+
+    if (document.getElementById('myonMyiconhomeseven').style.display == "block") {
+            document.getElementById('myonMyiconhomeseven').style.display = "none";
+            document.getElementById('P8').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhomeseven').style.display = "block";
+            document.getElementById('P8').style.color="#FFB600";
+
+    }
+    
+}
+
+function onMyiconhomeeight(){
+
+    if (document.getElementById('myonMyiconhomeeight').style.display == "block") {
+            document.getElementById('myonMyiconhomeeight').style.display = "none";
+            document.getElementById('P9').style.color="black";
+            
+    } else {
+            document.getElementById('myonMyiconhomeeight').style.display = "block";
+            document.getElementById('P9').style.color="#FFB600";
+
+    }
+    
+}
+
+
+function solidbarLasticon(){
+    if (document.getElementById('menu-bar-responsive').style.visibility == "visible") {
+            document.getElementById('menu-bar-responsive').style.visibility = "hidden";
+            
+    } else {
+
+                    document.getElementById('menu-bar-responsive').style.visibility = "visible"
+    }
+}
+
+
+
+// function changeColor(){
+//         document.getElementById('P2').style.color = "red";
+// }
+
+
